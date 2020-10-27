e13617eda7ac502f425bbce8c4f3f8c2b251bb99
tovietcuong <tovietcuong1999@gmail.com>
2020-10-28 01:33:28 +0700

    Hoan thanh model

--- src/Model/Tesst.java
+++ src/Model/Tesst.java
@@ -1,4 +1,10 @@
 package Model;
 
+import controller.Translator;
+
 public class Tesst {
+    public static void  main (String [] okok){
+        String toLangTxt = Translator.translateText( "en", "vi","Hello");
+        System.out.println(toLangTxt);
+    }
 }
